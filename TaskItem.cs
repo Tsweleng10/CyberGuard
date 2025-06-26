6a1a424f9e7e88693db75cc2205691e8b0ea3924
Tsweleng10 <st10451745@rcconnect.edu.za>
2025-06-26 14:47:53 +0000

    add task class

---
+++ TaskItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class TaskItem
+    {
+        public string Title {  get; set; }
+        public string Description { get; set; }
+        public DateTime? ReminderDate { get; set; }
+        public bool Completed { get; set; }
+
+        public void Display()
+        {
+            Console.WriteLine(Completed ? "[✔]" + Title : "[ ]" + Title);
+            Console.WriteLine("  Description: " + Description);
+            if (ReminderDate.HasValue)
+            {
+                Console.WriteLine("  Reminder: " + ReminderDate.Value.ToShortDateString());
+            }
+            Console.WriteLine();
+        }
+    }
+}
